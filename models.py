8010498b9e5b5e026a650f44e10d4d64d54330b3
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-08-03 20:15:58 +0200

    Create table

--- models.py
+++ models.py
@@ -1,49 +1,114 @@
-import json
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+    return conn
+
+if __name__ == '__main__':
+    create_connection(r"database.db")
+
+def execute_sql(conn, sql):
+    """ Execute sql
+    :param conn: Connection object
+    :param sql: a SQL script
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(sql)
+    except Error as e:
+        print(e)
 
 
-class Todos:
-    def __init__(self):
-        try:
-            with open("todos.json", "r") as f:
-                self.todos = json.load(f)
-        except FileNotFoundError:
-            self.todos = []
+if __name__ == "__main__":
+    create_projects_sql = """
+-- projects table
+CREATE TABLE IF NOT EXISTS projects (
+  id integer PRIMARY KEY,
+  title text NOT NULL,
+  description text,
+  year integer,
+  species text,
+  watch text
+);
+"""
 
-    def all(self):
-        return self.todos
+create_tasks_sql = """
+-- zadanie table
+CREATE TABLE IF NOT EXISTS tasks (
+  id integer PRIMARY KEY,
+  title text NOT NULL,
+  description VARCHAR(250) NOT NULL,
+  year integer,
+  species text NOT NULL,
+  watch text NOT NULL,
+  FOREIGN KEY (projekt_id) REFERENCES projects (id)
+);
+"""
 
-    def get(self, id):
-        return self.todos[id]
-        todo = [todo for todo in self.all() if todo['id'] == id]
-        if todo:
-            return todo[0]
-        return []
+db_file = "database.db"
 
-    def create(self, data):
-        self.todos.append(data)
-        self.save_all()
+conn = create_connection(db_file)
+if conn is not None:
+    execute_sql(conn, create_projects_sql)
+    execute_sql(conn, create_tasks_sql)
+    conn.close()
 
-    def save_all(self):
-        with open("todos.json", "w") as f:
-            json.dump(self.todos, f)
+def add_projekt(conn, projekt):
+    """
+    Create a new projekt into the projects table
+    :param conn:
+    :param projekt:
+    :return: projekt id
+    """
+    sql = '''INSERT INTO projects(title, description, year, species, watch)
+            VALUES(?,?,?,?,?)'''
+    cur = conn.cursor()
+    cur.execute(sql, projekt)
+    conn.commit()
+    return cur.lastrowid
 
-    def update(self, id, data):
-        todo = self.get(id)
-        if todo:
-            index = self.todos.index(todo)
-            self.todos[index] = data
-            self.save_all()
-            return True
-        return False
+def add_zadanie(conn, zadanie):
+    """
+    Create a new zadanie into the tasks table
+    :param conn:
+    :param zadanie:
+    :return: zadanie id
+    """
+    sql = '''INSERT INTO tasks(projekt_id, title, description, year, species, watch)
+            VALUES(?,?,?,?,?,?)'''
+    cur = conn.cursor()
+    cur.execute(sql, zadanie)
+    conn.commit()
+    return cur.lastrowid
 
-    
-    def delete(self, id):
-        todo = self.get(id)
-        if todo:
-            self.todos.remove(todo)
-            self.save_all()
-            return True
-        return False
+if __name__ == "__main__":
+   projekt = ("Title", "Description", "Year", "Species", "Watch")
 
+   conn = create_connection("database.db")
+   pr_id = add_projekt(conn, projekt)
 
-todos = Todos()
+   zadanie = (
+       pr_id,
+       "Save Private Ryan",
+       "War Drama",
+       "1998",
+       "Species",
+       "True"
+   )
+
+   zadanie_id = add_zadanie(conn, zadanie)
+
+   print(pr_id, zadanie_id)
+   conn.commit()
