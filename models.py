87b8189a64b12aec157a3bbfe03d5078b4ce1454
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-08-06 21:32:02 +0200

    Correct

--- models.py
+++ models.py
@@ -2,9 +2,13 @@ import json
 import sqlite3
 from sqlite3 import Error
 
+class DataBaseError(Exception):
+    pass
 
-class Todos:
-    def __init__ (self):
+
+class Projects:
+
+    def __init__(self):
         self.conn = self.create_connection("database.db")
         self.create_project()
 
@@ -22,21 +26,22 @@ class Todos:
             print(e)
         return conn
 
-    # if __name__ == '__main__':
-    #     create_connection(r"database.db")
-
-    def execute_sql(self, conn, sql):
-        """ Execute sql
-        :param conn: Connection object
+    def execute_sql(self, sql, data=None):
+        """
         :param sql: a SQL script
-        :return:
+        :param data: tuple
+        :return: cursor object
         """
         try:
-            c = conn.cursor()
-            c.execute(sql)
+            cur = self.conn.cursor()
+            if data:
+                cur.execute(sql, data)
+                self.conn.commit()
+            else:
+                cur.execute(sql)
+            return cur
         except Error as e:
-            print(e)
-
+            raise DataBaseError(f"Akcja nie powiodła sie z powodu: {e}")
 
     def create_project(self):
         create_projects_sql = """
@@ -51,84 +56,49 @@ class Todos:
         );
         """
 
-        self.execute_sql(self.conn, create_projects_sql)
-
-    def add_projekt(self, projekt):
-        """
-        Create a new projekt into the projects table
-        :param conn:
-        :param projekt:
-        :return: projekt id
-        """
-        sql = '''INSERT INTO projects(id, title, description, year, species, watch)
-            VALUES(?,?,?,?,?,?)'''
-        cur = self.conn.cursor()
-        cur.execute(sql, projekt)
-        self.conn.commit()
-        return cur.lastrowid
-
+        self.execute_sql(create_projects_sql)
 
     def all(self):
-        select_all_sql = "SELECT * FROM projects"
-        cur = self.conn.cursor()
-        cur.execute(select_all_sql)
-        rows = cur.fetchall()
-        return rows
-    
-    def one(self):
-        select_one_sql = "SELECT * FROM projects"
-        cur = self.conn.cursor()
-        cur.execute(select_one_sql)
-        rows = cur.fetchone()
-        return rows
+        sql = "SELECT * FROM projects"
+        projects = self.execute_sql(sql)
+        return projects.fetchall()
 
-    def update(self):
-        """
-        update status, begin_date, and end date of a task
-        :param conn:
-        :param projekt:
-        :return: projekt id
-        """
+    def details(self, id):
+        sql = "SELECT * FROM projects WHERE id = ?"
+        project = self.execute_sql(sql, (id,))
+        return project.fetchone()
 
-        sql = '''UPDATE projects
-        SET title = 'Intouchables',
-            description = 'Touching story of a man helping a disabled person',
-            year   = '2011',
-            species = 'Biographical'
-        WHERE id = 3;'''
+    def create(self, project):
+        sql = "INSERT INTO projects(id, title, description, year, species, watch) VALUES(?,?,?,?,?,?)"
+        try:
+            self.execute_sql(sql, project)
+            return project
 
-        cur = self.conn.cursor()
-        cur.execute(sql)
-        self.conn.commit()
-        return cur.lastrowid
-    
-    def delete(self):
-        sql = '''DELETE FROM projects WHERE id = 3'''
+        except DataBaseError as e:
+            return [str(e)]
 
-        cur = self.conn.cursor()
-        cur.execute(sql)
-        self.conn.commit()
-        return cur.lastrowid
+    def update(self, id, project):
+        sql = """
+        UPDATE projects
+            SET title = ?,
+                description = ?,
+                year = ?,
+                species = ?,
+                watch = ?
+                WHERE id = ?;"""
+        data = project + (id, )
+        cur = self.execute_sql(sql, data)
+        return data
 
-        
+    def delete(self, id):
+        sql = "DELETE FROM projects WHERE id = ?"
+        self.execute_sql(sql, (id, ))
+        return True
 
-    def get(self, id):
-        return self.todos[id]
-        todo = [todo for todo in self.all() if todo['id'] == id]
-        if todo:
-            return todo[0]
-        return []
+projects = Projects()
 
-    def create(self, data):
-        self.todos.append(data)
-        self.save_all()
+if __name__ == "__main__":
+    print(projects.all())
 
-    def save_all(self):
-        with open("todos.json", "w") as f:
-            json.dump(self.todos, f)
 
-todos = Todos() 
 
-# todos.add_projekt((3, "Save Private Ryan", "War Drama", 1998, "War Drama", "True"))
-
-print(todos.delete())
